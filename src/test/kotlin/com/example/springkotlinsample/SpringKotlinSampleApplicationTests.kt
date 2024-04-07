064240f76a8f2d245bf5b5a33916ded5980ec6d2
leo <leonardomatiasrodriguez@gmail.com>
2024-04-06 23:54:23 -0300

    uploading project

---
+++ src/test/kotlin/com/example/springkotlinsample/SpringKotlinSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springkotlinsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
